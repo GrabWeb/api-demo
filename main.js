e1af68638a27efaf510294530a3b9443e3a0fc93
haozhuang <1340335269@qq.com>
2019-05-18 10:38:32 +0800

    api

---
+++ main.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    function getSiblings(node){    
+        var allChildren = node.parentNode.children
+        var array = {
+        length: 0
+        }
+        for(let i = 0; i < allChildren.length; i++){
+            if(allChildren[i] !== node){
+                array[array.length] = allChildren[i]
+                array.length += 1
+            }
+        }
+        return array
+    }
+    console.log(getSiblings(item3))
+    
+    function addClass(node, classes){
+        for(let key in classes){
+            var value = classes[key]
+            if (value) {
+                node.classList.add(key)
+            } else {
+                 node.classList.remove(key)
+            }
+        }
+    }
+    addClass(item3, {a: true, b: false, c: true})
+}
